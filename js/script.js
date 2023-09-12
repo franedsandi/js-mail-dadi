416e20f0603cbd83d635096672f0e53c7c33ba5a
franedsandi <franedsandi@gmail.com>
2023-09-12 16:38:12 +0200

    [feat] email picker and checker + welcome message

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-/* invited emails */
-const invited = ['hello@hotmail.com','hi@hotmail.com','Inter@hotmail.com','youtube@hotmail.com','google@hotmail.com','pavia@hotmail.com','hello@gmail.com','hi@gmail.com','Inter@gmail.com','youtube@gmail.com','google@gmail.com','pavia@gmail.com'];
-console.log (invited);
-const email =  document.getElementById('email');
-const button1 = document.getElementById('btn-1');
-const outputmessage =  document.getElementById('outputmessage');
-button1.addEventListener('click', function(){
-if (email == invited){
-  document.outputmessage.innerHTML = holi;
-}
-})
+const invited = ['hello@hotmail.com', 'hi@hotmail.com', 'Inter@hotmail.com','youtube@hotmail.com', 'google@hotmail.com', 'pavia@hotmail.com', 'hello@gmail.com', 'hi@gmail.com', 'Inter@gmail.com', 'youtube@gmail.com', 'google@gmail.com', 'pavia@gmail.com']; 
+
+document.getElementById('btn-1').addEventListener('click', function() {
+    const emailInput = document.getElementById('email').value;
+    const outputWelcome = document.querySelector('.outputwelcome');
+    const picker = document.querySelector('.emailpicker');
+
+    if (invited.includes(emailInput)) {
+        outputWelcome.textContent = 'Welcome! ' + emailInput + ' we were waiting for you!';
+        picker.classList.add('d-none');
+    } else {
+        outputWelcome.textContent = 'Invalid email. You are not invited.';
+    }
+});
+
