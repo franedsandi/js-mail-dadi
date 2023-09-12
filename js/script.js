e397097229a36884d782e8e1a9a161b644879a51
franedsandi <franedsandi@gmail.com>
2023-09-12 19:21:31 +0200

    [feat] coments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /* invited list */
 const invited = {
-  'franedsandi@gmail.com': true,
   'hello@hotmail.com': true,
   'hi@hotmail.com': true,
   'Inter@hotmail.com': true,
@@ -23,8 +22,9 @@ button1.addEventListener('click', function() {
     const emailInput = document.getElementById('email').value;
     const outputWelcome = document.querySelector('.outputwelcome');
     const picker = document.querySelector('.emailpicker');
-
+/* email checker */
     if (invited[emailInput]) {
+      /* email correct */
       const userEmail = emailInput;
         outputWelcome.textContent = 'Welcome! ' + userEmail + ' we were waiting for you!';
         picker.classList.add('d-none');
@@ -38,13 +38,14 @@ button1.addEventListener('click', function() {
           button3.classList.remove('d-none'); // Mostrar btn-3 al finalizar la ronda
         })
     } else {
-        outputWelcome.textContent = 'Invalid email. You are not invited.';
+      /* email incorrect */
+        outputWelcome.textContent = 'sorry, you are not invited , get away!';
     }
 });
 const dadiContainer = document.querySelector('.dadi');
 let firstBoxValue;
 let secondBoxValue;
-
+/* game card */
 for (let i = 0; i <= 1; i++) {
   const boxContainer = document.createElement('div');
   boxContainer.classList.add('box-container');
@@ -53,34 +54,39 @@ for (let i = 0; i <= 1; i++) {
   const box = document.createElement('div');
   box.classList.add('box');
   boxContainer.append(box);
-
+/* random number generator */
   const min = 1;
   const max = 6;
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   box.textContent = randomNumber;
-
+/* user random number */
   if (i === 0) {
+    /* user random number */
     firstBoxValue = randomNumber;
     const userText = document.createElement('div');
     userText.textContent = 'you';
     boxContainer.append(userText);
   } else {
+    /* pc random number */
     secondBoxValue = randomNumber;
     const pcText = document.createElement('div');
     pcText.textContent = 'casino';
     boxContainer.append(pcText);
   }
 }
-
+/* random number coimperation */
 const winnerMessage = document.querySelector('.winner');
 if (firstBoxValue > secondBoxValue) {
+  /* if user wins */
   winnerMessage.textContent ='You win!';
 } else if (firstBoxValue < secondBoxValue) {
+  /* if pc wins */
   winnerMessage.textContent = 'The most boring Casino online wins!';
 } else {
+   /* if its a tie */
   winnerMessage.textContent = "It's a tie!";
 }
-
+/* retry */
 button3.addEventListener('click', function() {
   dadiContainer.innerHTML = ''; 
   for (let i = 0; i <= 1; i++) {
